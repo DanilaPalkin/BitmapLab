ae5ca027343ab5ae129d69a7e0c5101226e06902
DanilaPalkin <danilapalkin@gmail.com>
2022-03-26 21:10:19 +0300

    Bitmaps + Win Forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace scoi_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 330);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Изображение 1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбрать изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 358);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 330);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Изображение 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выбрать изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(15, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(270, 270);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.panel1);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(334, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(562, 676);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Что сделаем с изображениями?";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(233, 645);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 25);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Сохранить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(16, 108);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(530, 530);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(446, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Поехали!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(187, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Изменить цвет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(161, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(20, 20);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 62);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Наложить маску?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 400;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Попиксельная сумма",
+            "Попиксельное произведение",
+            "Среднее-арифметическое",
+            "Минимум",
+            "Максимум"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(530, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 711);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox2;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private GroupBox groupBox3;
+        private ComboBox comboBox1;
+        private Panel panel1;
+        private CheckBox checkBox1;
+        private Button button3;
+        private ColorDialog colorDialog1;
+        private Button button4;
+        private Button button5;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+namespace scoi_lab1
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap image1 = null;
+        private Bitmap image2 = null;
+        private Bitmap image3 = null;
+        public Form1()
+        {
+            InitializeComponent();
+            image1 = new Bitmap(500, 500);
+            pictureBox1.Image = image1;
+            image2 = new Bitmap(500, 500);
+            pictureBox2.Image = image2;
+            image3 = new Bitmap(500, 500);
+            pictureBox3.Image = image3;
+        }
+
+        // Выбор первой картинки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "Картинки (png, jpg, bmp, gif) |*.png;*.jpg;*.bmp;*.gif|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (image1 != null)
+                {
+                    pictureBox1.Image = null;
+                    image1.Dispose();
+                }
+
+                image1 = new Bitmap(openFileDialog.FileName);
+                pictureBox1.Image = image1;
+            }
+        }
+
+        // Выбор второй картинки
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "Картинки (png, jpg, bmp, gif) |*.png;*.jpg;*.bmp;*.gif|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (image2 != null)
+                {
+                    pictureBox2.Image = null;
+                    image2.Dispose();
+                }
+
+                image2 = new Bitmap(openFileDialog.FileName);
+                pictureBox2.Image = image2;
+            }
+        }
+
+        // Эта кнопка отвечает за выбор цвета маски
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult colorResult = colorDialog1.ShowDialog();
+            if (colorResult == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button3.Enabled = true;
+            } else
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+        }
+
+        // Кнопка обработки изображений
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = true;
+            for (int i = 0; i < 500; ++i)
+            {
+                for (int j = 0; j < 500; ++j)
+                {
+                    var pix1 = image1.GetPixel(j, i); // Цвет пикселя с первой картинки
+                    int r1 = pix1.R;
+                    int g1 = pix1.G;
+                    int b1 = pix1.B;
+                    var pix2 = image2.GetPixel(j, i); // Цвет пикселя со второй картинки
+                    int r2 = pix2.R;
+                    int g2 = pix2.G;
+                    int b2 = pix2.B;
+
+                    switch (comboBox1.SelectedIndex)
+                    {
+                        // сумма
+                        case 0:
+                            int r3 = (int)Clamp(r1 + r2, 0, 255);
+                            int g3 = (int)Clamp(g1 + g2, 0, 255);
+                            int b3 = (int)Clamp(b1 + b2, 0, 255);
+                            var pix3 = Color.FromArgb(r3, g3, b3);
+                            image3.SetPixel(j, i, pix3);
+                            break;
+                        // произведение
+                        case 1:
+                            r3 = r1 * r2 / 255;
+                            g3 = g1 * g2 / 255;
+                            b3 = b1 * b2 / 255;
+                            pix3 = Color.FromArgb(r3, g3, b3);
+                            image3.SetPixel(j, i, pix3);
+                            break;
+                        // среднее-арифметическое
+                        case 2:
+                            r3 = (r1 + r2) / 2;
+                            g3 = (g1 + g2) / 2;
+                            b3 = (b1 + b2) / 2;
+                            pix3 = Color.FromArgb(r3, g3, b3);
+                            image3.SetPixel(j, i, pix3);
+                            break;
+                        // минимум
+                        case 3:
+                            r3 = Math.Min(r1, r2);
+                            g3 = Math.Min(g1, g2);
+                            b3 = Math.Min(b1, b2);
+                            pix3 = Color.FromArgb(r3, g3, b3);
+                            image3.SetPixel(j, i, pix3);
+                            break;
+                        // максимум
+                        case 4:
+                            r3 = Math.Max(r1, r2);
+                            g3 = Math.Max(g1, g2);
+                            b3 = Math.Max(b1, b2);
+                            pix3 = Color.FromArgb(r3, g3, b3);
+                            image3.SetPixel(j, i, pix3);
+                            break;
+                    }
+                }
+            }
+
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < 500; ++i)
+                {
+                    for (int j = 0; j < 500; ++j)
+                    {
+                        var pix3 = image3.GetPixel(j, i); // Цвет пикселя с третьей картинки
+                        int r3 = pix3.R;
+                        int g3 = pix3.G;
+                        int b3 = pix3.B;
+                        r3 = r3 * panel1.BackColor.R / 255;
+                        g3 = g3 * panel1.BackColor.G / 255;
+                        b3 = b3 * panel1.BackColor.B / 255;
+                        pix3 = Color.FromArgb(r3, g3, b3);
+                        image3.SetPixel(j, i, pix3);
+                    }
+                }
+            }
+            pictureBox3.Image = image3;
+        }
+
+        // Кнопка сохранения
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using SaveFileDialog saveFileFialog = new SaveFileDialog();
+            saveFileFialog.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileFialog.Filter = "Картинки (png, jpg, bmp, gif) |*.png;*.jpg;*.bmp;*.gif|All files (*.*)|*.*";
+            saveFileFialog.RestoreDirectory = true;
+
+            if (saveFileFialog.ShowDialog() == DialogResult.OK)
+            {
+                if (image3 != null)
+                {
+                    image3.Save(saveFileFialog.FileName);
+                }
+            }
+        }
+
+        public static T Clamp<T>(T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+    }
+}
