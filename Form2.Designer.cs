516dda986f9cf69e0084174a26714ff0396eddbe
DanilaPalkin <danilapalkin@gmail.com>
2022-04-10 17:54:11 +0300

    Добавлена яркость и негатив
    При сильном смещении гистограммы в стороны ломается её высота

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,19 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 318);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(697, 318);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ярче";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Темнее";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Контрастность выше";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(125, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Контрастность ниже";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(409, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(85, 124);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Негатив";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(721, 648);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private PictureBox pictureBox2;
+        private CheckBox checkBox1;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -13,24 +13,27 @@ namespace scoi_lab1
     public partial class Form2 : Form
     {
         int[] histogram;
-        public Form2(Bitmap image)
+        private Bitmap image = null;
+        public Form2(Bitmap imageFromForm1)
         {
             InitializeComponent();
+            image = imageFromForm1;
+            pictureBox2.Image = image;
             histogram = GetHistogram(image);
-            this.Paint += (o, e) => DrawHistogram(e.Graphics, ClientRectangle, histogram);
-            this.Resize += (o, e) => Refresh();
+            pictureBox1.Paint += (o, e) => DrawHistogram(e.Graphics, pictureBox1, histogram);
+            pictureBox1.Resize += (o, e) => Refresh();
             Refresh();
         }
 
-        private static void DrawHistogram(Graphics g, Rectangle rectangle, int[] histogram)
+        private static void DrawHistogram(Graphics g, PictureBox pictureBox1, int[] histogram)
         {
             float max = histogram.Max();
             if (max > 0)
             {
                 for(int i = 0; i < histogram.Length; i++)
                 {
-                    float h = rectangle.Height * histogram[i] / (float)max;
-                    g.FillRectangle(Brushes.Black, i * rectangle.Width / (float)histogram.Length, rectangle.Height - h, rectangle.Width / (float)histogram.Length, h);
+                    float h = pictureBox1.Height * histogram[i] / (float)max;
+                    g.FillRectangle(Brushes.Black, i * pictureBox1.Width / (float)histogram.Length, pictureBox1.Height - h, pictureBox1.Width / (float)histogram.Length, h);
                 }
             }
         }
@@ -47,6 +50,100 @@ namespace scoi_lab1
                 }
             }
             return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 500; ++i)
+            {
+                for (int j = 0; j < 500; ++j)
+                {
+                    var pix = image.GetPixel(j, i); // Цвет пикселя
+                    int r = pix.R;
+                    int g = pix.G;
+                    int b = pix.B;
+                    r = r + 15;
+                    g = g + 15;
+                    b = b + 15;
+                    if(r > 255)
+                        r = 255;
+                    if(g > 255)
+                        g = 255;
+                    if(b > 255)
+                        b = 255;
+                    pix = Color.FromArgb(r, g, b);
+                    image.SetPixel(j, i, pix);
+                }
+            }
+            pictureBox2.Image = image;
+            histogram = GetHistogram(image);
+            pictureBox2.Refresh();
+            pictureBox1.Paint += (o, e) => DrawHistogram(e.Graphics, pictureBox1, histogram);
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 500; ++i)
+            {
+                for (int j = 0; j < 500; ++j)
+                {
+                    var pix = image.GetPixel(j, i); // Цвет пикселя
+                    int r = pix.R;
+                    int g = pix.G;
+                    int b = pix.B;
+                    r = r - 15;
+                    g = g - 15;
+                    b = b - 15;
+                    if (r < 0)
+                        r = 0;
+                    if (g < 0)
+                        g = 0;
+                    if (b < 0)
+                        b = 0;
+                    pix = Color.FromArgb(r, g, b);
+                    image.SetPixel(j, i, pix);
+                }
+            }
+            pictureBox2.Image = image;
+            histogram = GetHistogram(image);
+            pictureBox2.Refresh();
+            pictureBox1.Paint += (o, e) => DrawHistogram(e.Graphics, pictureBox1, histogram);
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 500; ++i)
+            {
+                for (int j = 0; j < 500; ++j)
+                {
+                    var pix = image.GetPixel(j, i); // Цвет пикселя
+                    int r = pix.R;
+                    int g = pix.G;
+                    int b = pix.B;
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+                    pix = Color.FromArgb(r, g, b);
+                    image.SetPixel(j, i, pix);
+                }
+            }
+            pictureBox2.Image = image;
+            histogram = GetHistogram(image);
+            pictureBox2.Refresh();
+            pictureBox1.Paint += (o, e) => DrawHistogram(e.Graphics, pictureBox1, histogram);
+            pictureBox1.Refresh();
         }
     }
 }
